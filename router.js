75e26cb01786654d7c94a2dd5390c3144e507f3d
Amit Dekate <amitdekate@hotmail.com>
2013-04-10 16:05:25 -0700

    Revert "add router"
    This reverts commit 1f9a44d5aa77d9d129a346f2a386ad9488673335.

--- router.js
+++
@@ -1,5 +0,0 @@
-function route(pathname, response) {
-  response.write("About to route a request for " + pathname);
-}
-
-exports.route = route;
--- server.js
+++ server.js
@@ -3,16 +3,12 @@ var url = require("url");
 
 var port = process.env.PORT || 1337;
 
-function start(route) {
+function start() {
   function onRequest(request, response) {
-    var pathname = url.parse(request.url).pathname;
-
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-
-    route(pathname, response);
-
-    response.end();
+	var pathname = url.parse(request.url).pathname;
+    	response.writeHead(200, {"Content-Type": "text/plain"});
+    	response.write("Hello World" + pathname);
+    	response.end();
   }
 
   http.createServer(onRequest).listen(port);
