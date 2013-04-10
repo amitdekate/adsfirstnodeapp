157a66680aadb70049b3e972487671313e40dbcb
Amit Dekate <amitdekate@hotmail.com>
2013-04-10 15:51:05 -0700

    remove console

--- server.js
+++ server.js
@@ -3,19 +3,15 @@ var url = require("url");
 
 var port = process.env.PORT || 1337;
 
-console.log("this is a test");
-
 function start() {
   function onRequest(request, response) {
 	var pathname = url.parse(request.url).pathname;
-	console.log("Request for " + pathname + " received.");
     	response.writeHead(200, {"Content-Type": "text/plain"});
-    	response.write("Hello World");
+    	response.write("Hello World" + pathname);
     	response.end();
   }
 
   http.createServer(onRequest).listen(port);
-  console.log("Server has started.");
 }
 
 exports.start = start;
