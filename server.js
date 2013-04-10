ed7f9e5d95df05f740b8330012610703b60a4bcb
Amit Dekate <amitdekate@hotmail.com>
2013-04-10 16:07:21 -0700

    add router

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ var url = require("url");
 
 var port = process.env.PORT || 1337;
 
-function start() {
+function start(route) {
   function onRequest(request, response) {
-	var pathname = url.parse(request.url).pathname;
-    	response.writeHead(200, {"Content-Type": "text/plain"});
-    	response.write("Hello World" + pathname);
-    	response.end();
+    var pathname = url.parse(request.url).pathname;
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World");
+
+    route(pathname, response);
+
+    response.end();
   }
 
   http.createServer(onRequest).listen(port);
